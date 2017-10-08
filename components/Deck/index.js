7cbba05c6ffac7e4ba484d7d24732af1b93a7c59
Luiz Figueiredo <lerfbr@gmail.com>
2017-10-08 18:24:23 -0300

    feat: Basic Components

---
+++ components/Deck/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+export default const Deck = () => {
+  return (
+    <View>
+      <Text>I'm a Deck</Text>
+    </View>
+  )
+}
